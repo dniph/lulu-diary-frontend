c20f04eeaef764f9e480d3796bf5c3c4144199f6
Dennif <dennif.pa@gmail.com>
2025-08-05 22:30:48 -0700

    Diary screen ready

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -5,7 +5,15 @@ import DiaryEntry from "@/components/DiaryEntry";
 
 export default function DiaryPage() {
   return (
-    <div>
+    <div className="min-h-screen relative overflow-hidden font-kawaii">
+            <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: "url('/images/ATARDECER.JPG')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      ></div>
       <DiaryEntry />
     </div>
   );
